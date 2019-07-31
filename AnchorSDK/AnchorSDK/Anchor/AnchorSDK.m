d81458f4e4015711b245c7e991af5b8bb2ac8f4d
chenlei <leichensix@163.com>
2019-07-31 16:35:46 +0800

    1.0.1

--- AnchorSDK/AnchorSDK/Anchor/AnchorSDK.m
+++ AnchorSDK/AnchorSDK/Anchor/AnchorSDK.m
@@ -15,6 +15,9 @@
 
 static AnchorConfig *_defaultConfig;
 
+static dispatch_source_t _timer;
+
+#define WE_APP_START_TIME @"we_app_start_time"
 
 @implementation AnchorSDK
 
@@ -40,8 +43,78 @@ static AnchorConfig *_defaultConfig;
             [AnchorSDK initUMengWithConfig:config];
         }
     }
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onApplicationDidBecomeActive) name:UIApplicationDidBecomeActiveNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onApplicationWillResignActive) name:UIApplicationWillResignActiveNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onApplicationWillTerminate) name:UIApplicationWillTerminateNotification object:nil];
 }
 
++ (void)onApplicationDidBecomeActive {
+    [[WEEventManager shareManager] activeTrack];
+    
+    //打开APP
+    [[WEEventManager shareManager] trackEvent:WE_APP_START];
+    
+    //首次打开
+    if ([AnchorUtil isFirstInstall]) {
+        [[WEEventManager shareManager] trackEvent:WE_FIRST_INSTALL];
+    }
+    
+    //是否越狱
+    NSString *isJailBreak =  [NSString stringWithFormat:@"%d", [AnchorUtil isJailBreak]];
+    NSLog(@"isJailBreak: %@", isJailBreak);
+    [[WEEventManager shareManager] trackEvent:WE_JUDGE value:@{@"description": @"1", @"result":isJailBreak}];
+    
+    //获取设备设置时区与GMT之前的差值
+    NSString *seconds = [AnchorUtil secondsFromGMTForDate];
+    [[WEEventManager shareManager] trackEvent:WE_OFFSET_ACQUIRE value:@{@"value": seconds}];
+    
+    [AnchorSDK startTimer];
+}
+
++ (void)onApplicationWillResignActive {
+    [AnchorSDK stopTimer];
+}
+
++ (void)onApplicationWillTerminate {
+    [self stopTimer];
+}
+
+
++ (void)startTimer {
+    //记录app进入活跃状态的时间
+    [[NSUserDefaults standardUserDefaults] setValue:[NSDate date] forKey:WE_APP_START_TIME];
+    
+    //设置时间间隔
+    NSTimeInterval period = 600.f;
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    _timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
+    dispatch_source_set_timer(_timer, dispatch_walltime(NULL, 0), period * NSEC_PER_SEC, 0);
+    // 事件回调
+    dispatch_source_set_event_handler(_timer, ^{
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSLog(@"time out");
+            //上报
+            [[WEEventManager shareManager] trackEvent:WE_ENGAGEMENT value:@{@"time": @"600"}];
+            //上报一次后更新app进入活跃状态的时间
+            [[NSUserDefaults standardUserDefaults] setValue:[NSDate date] forKey:WE_APP_START_TIME];
+        });
+    });
+    
+    // 开启定时器
+    dispatch_resume(_timer);
+}
+
++ (void)stopTimer {
+    dispatch_source_cancel(_timer);
+    if ([[NSUserDefaults standardUserDefaults] valueForKey:WE_APP_START_TIME]) {
+        NSDate *startDate = [[NSUserDefaults standardUserDefaults] valueForKey:WE_APP_START_TIME];
+        NSDate *stopDate = [NSDate date];
+        NSTimeInterval time = [stopDate timeIntervalSinceDate:startDate];
+        NSString *timeStr = [NSString stringWithFormat:@"%.f", time];
+        [[WEEventManager shareManager] trackEvent:WE_ENGAGEMENT value:@{@"time": timeStr}];
+    }
+}
 
 
 + (void)initFirebase {
